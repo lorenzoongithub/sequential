cd708601722317ea89c3b1dfa7527e43330dd847
lorenzo puccetti <lorenzo.puccetti@gmail.com>
2017-06-17 17:02:16 +0100

    use lz-string to store snippet.

--- docs/locationr.js
+++ docs/locationr.js
@@ -1,26 +1,30 @@
 /**
- * A wrapper for window.location to provide a basic implementation for JSON + URL encoding
+ * A wrapper for window.location to provide a basic implementation to encode/decode a JavaScript Object
+ * (which can be jsonified).
+ * It depends on LZString
  * 
+ * The first character after the question mark "?" is the letter "Z". This is a minimal attempt to give us room for upgrades on  
+ * encoding/decoding algorithm.		 
 **/
 locationr = {
 	read : function() {
-		var search = location.search.substring(1);
+		var search = location.search.substring(2); // assume it starts with ?Z
 		try {
-			return JSON.parse(decodeURIComponent(search));
+			return JSON.parse(LZString.decompressFromEncodedURIComponent(search));
 		} catch (e) {
-			return null;
+			return null; // returns null in any exceptional case: eg. URL which could not be decompressed or not a valid JSON string representation.  
 		}
 	},
 	linkify : function(oj,page) {
 		var str = JSON.stringify(oj);
-		return page+'?'+encodeURIComponent(str);
+		return page+'?Z'+LZString.compressToEncodedURIComponent(str);
 	},
 	write : function(oj,page) {
 		var str = JSON.stringify(oj);
-		location.href = page+'?'+encodeURIComponent(str);	
+		location.href = page+'?Z'+LZString.compressToEncodedURIComponent(str);
 	},
 	replace : function(oj,page) {
 		var str = JSON.stringify(oj);
-		location.replace(page+'?'+encodeURIComponent(str)); 
+		location.replace(page+'?Z'+LZString.compressToEncodedURIComponent(str)); 
 	}
-}
+};
